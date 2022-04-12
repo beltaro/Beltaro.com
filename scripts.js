27f620f87747707c5c5d2df31aff44fc51e7de4b
beltaro <mehdi.f.azad@gmail.com>
2022-04-12 17:50:41 +0430

    Submit button and terms check

--- scripts.js
+++ scripts.js
@@ -1,4 +1,16 @@
 function myFunction() {
     window.alert("Welcome ! This Is A js Test !")
 }
+function agreeEua() {
+    // checks if the user has agreed to the terms and conditions ; if so will display the button for submition
+    let check = true
+    if (check) {
+        Document.getElementById("Eua").style.display = "block";
+        check = false;
+    };
+    if (!check) {
+        Document.getElementById("Eua").style.display = "none";
+        check = true;
+    };
+} 
 myFunction()
