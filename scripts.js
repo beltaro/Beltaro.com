72d3dc3a631a5169c94e5023432e2f4b02c9ae46
beltaro <mehdi.f.azad@gmail.com>
2022-04-12 17:14:47 +0430

    Update

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
 function myFunction() {
     window.alert("Welcome ! This Is A js Test !")
 }
+function submitFunction() {
+    var checkBox = document.getElementById("Checkbox-submit");
+    var text = document.getElementById("Submit-button");
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
 myFunction()
+submitFunction()
