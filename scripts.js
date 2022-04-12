dd435042215ea363cdb2bc79c496284a00365522
beltaro <mehdi.f.azad@gmail.com>
2022-04-12 17:20:39 +0430

    Undid Some Shit

--- scripts.js
+++ scripts.js
@@ -1,14 +1,4 @@
 function myFunction() {
     window.alert("Welcome ! This Is A js Test !")
 }
-function submitFunction() {
-    var checkBox = document.getElementById("Checkbox-submit");
-    var Submit-button = document.getElementById("Submit-button");
-    if (checkBox.checked == true){
-        Submit-button.style.display = "block";
-    } else {
-        Submit-button.style.display = "none";
-    }
-}
 myFunction()
-submitFunction()
